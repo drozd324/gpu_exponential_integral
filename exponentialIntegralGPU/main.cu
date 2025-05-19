c66e5f6ee9afb79b4e3132b9b7dcce4223cc85e3
Patryk Drozd <drozdpk@gmail.com>
2025-05-19 11:38:05 +0100

    good data?

--- exponentialIntegralGPU/main.cu
+++ exponentialIntegralGPU/main.cu
@@ -172,8 +172,11 @@ int main(int argc, char *argv[]){
 	}
 
 	if (csv){
-		//"time_cpu_float,time_cpu_double,block_size,time_gpu_float,time_gpu_double,diff_float,diff_double,spdup_float,spdup_double" 
+		//"n,numberOfSamples,time_cpu_float,time_cpu_double,block_size,time_gpu_float,time_gpu_double,diff_float,diff_double,spdup_float,spdup_double,cpu,gpu" 
 		int none = -9999;	
+
+		std::cout << n << "," << numberOfSamples << ",";		
+		
 
 		if (cpu){
 			std::cout << timeTotalCpuFloat << "," << timeTotalCpuDouble << ",";		
@@ -202,6 +205,9 @@ int main(int argc, char *argv[]){
 			std::cout << none << ",";
 			std::cout << none << ",";
 		}
+
+		std::cout << cpu << ",";
+		std::cout << gpu << ",";
 		
 		std::cout << std::endl;
 	}
--- exponentialIntegralGPU/runme.sh
+++ exponentialIntegralGPU/runme.sh
@@ -6,18 +6,17 @@ SIZES=(5000 8192 16384 20000)
 
 for (( j=0; j<4; j++ )); do
 	FILE_NAME="out${SIZES[$j]}"
-	echo "time_cpu_float,time_cpu_double,block_size,time_gpu_float,time_gpu_double,diff_float,diff_double,spdup_float,spdup_double" > $FILE_NAME
+	echo "n,numberOfSamples,time_cpu_float,time_cpu_double,block_size,time_gpu_float,time_gpu_double,diff_float,diff_double,spdup_float,spdup_double,cpu,gpu" > $FILE_NAME
+		
 	
-	FLAGS_CPU="-g -r -B" 
+	FLAGS_CPU="-g -r" 
 	echo "./main $FLAGS_CPU -n ${SIZES[$j]} -m ${SIZES[$j]} >> $FILE_NAME"
-	./main $FLAGS_CPU -n ${SIZES[$j]} -m ${SIZES[$j]} >> $FILE_NAME
 	./main $FLAGS_CPU -n ${SIZES[$j]} -m ${SIZES[$j]} >> $FILE_NAME
 
 	for (( i=1; i<33; i++ )); do
 
 		FLAGS_GPU="-c -r -B $i " 
 		echo "./main $FLAGS_GPU -n ${SIZES[$j]} -m ${SIZES[$j]} >> $FILE_NAME"
-		./main $FLAGS_GPU -n ${SIZES[$j]} -m ${SIZES[$j]} >> $FILE_NAME
 		./main $FLAGS_GPU -n ${SIZES[$j]} -m ${SIZES[$j]} >> $FILE_NAME
 	done
 done
